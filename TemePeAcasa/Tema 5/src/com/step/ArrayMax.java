048473f5f214fbeef5866c5a7ac2227b8e6f5e7b
Arty Rotaru <eusuntra@gmail.com>
2020-10-10 01:29:09 +0300

    Tema pe acasa 5 - 3exerciti u

--- TemePeAcasa/Tema 5/src/com/step/ArrayMax.java
+++ TemePeAcasa/Tema 5/src/com/step/ArrayMax.java
@@ -16,34 +16,5 @@ public class ArrayMax {
         for(int i=0; i<unArray.length; i++ ) {
             unArray[i] = elemente.nextInt();
         }
-        // Calling getMax() method for getting max value
-        int max = getMax(unArray);
-        System.out.println("Maximum Value is: "+max);
 
-        // Calling getMin() method for getting min value
-        int min = getMin(unArray);
-        System.out.println("Minimum Value is: "+min);
-    }
-
-    // Method for getting the maximum value
-    public static int getMax(int[] inputArray){
-        int maxValue = inputArray[0];
-        for(int i=1;i < inputArray.length;i++){
-            if(inputArray[i] > maxValue){
-                maxValue = inputArray[i];
-            }
-        }
-        return maxValue;
-    }
-
-    // Method for getting the minimum value
-    public static int getMin(int[] inputArray){
-        int minValue = inputArray[0];
-        for(int i=1;i<inputArray.length;i++){
-            if(inputArray[i] < minValue){
-                minValue = inputArray[i];
-            }
-        }
-        return minValue;
-    }
 }
