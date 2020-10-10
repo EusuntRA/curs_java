6d66dfb8c65d284f9f4ca8584ebdd11dafd13cf5
Arty Rotaru <eusuntra@gmail.com>
2020-10-10 03:28:06 +0300

    Tema pe acasa 5 - 5 exerciti

--- TemePeAcasa/Tema 5/src/com/step/Calculator.java
+++ TemePeAcasa/Tema 5/src/com/step/Calculator.java
@@ -5,5 +5,63 @@
 
 package com.step;
 
-public class Calculator {
+class Calculator {
+
+            public int sum(int num1, int num2) {
+                return (num1 + num2);
+            }
+            public double sum(double num1, double num2) {
+                return (num1 + num2);
+            }
+            public double sum(int num1, double num2) {
+                return (num1 + num2);
+            }
+            public double sum(double num1, int num2) {
+                return (num1 + num2);
+            }
+
+
+
+            public int min(int num1, int num2) {
+                return (num1 - num2);
+            }
+            public double min(double num1, double num2) {
+                return (num1 - num2);
+            }
+            public double min(int num1, double num2) {
+                return (num1 - num2);
+            }
+            public double min(double num1, int num2) {
+                return (num1 - num2);
+            }
+
+
+
+            public int mult(int num1, int num2) {
+                return (num1 * num2);
+            }
+            public double mult(double num1, double num2) {
+                return (num1 * num2);
+            }
+            public double mult(int num1, double num2) {
+                return (num1 * num2);
+            }
+            public double mult(double num1, int num2) {
+                return (num1 * num2);
+            }
+
+
+
+            public int divi(int num1, int num2) {
+                return (num1 / num2);
+            }
+            public double divid(double num1, double num2) {
+                return (num1 / num2);
+            }
+            public double divi(int num1, double num2) {
+                return (num1 / num2);
+            }
+            public double divid(int num1, double num2) {
+                return (num1 / num2);
+            }
 }
